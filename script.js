1bec0777d138676536faa4a3a3871a4104417c0c
Miguelcmd <miguelangel-0921@live.com>
2024-10-09 19:00:20 -0500

    Cambio de fondo seccion2

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ function updateFloatingBox(scrollPos) {
 
     // Cambia el contenido, posición, color del texto y la fuente basados en la sección
     if (section1Top <= window.innerHeight / 2 && section1Top > -section1.clientHeight / 2) {
-        floatingBox.innerHTML = 'Sección actual: Domicilios';
-        floatingBox.style.top = '10%'; // Cambia la posición top
-        floatingBox.style.left = '20px'; // Cambia la posición left
+        floatingBox.innerHTML = 'Domicilios';
+        floatingBox.style.top = '30%'; // Cambia la posición top
+        floatingBox.style.left = '55%'; // Cmbia la posición left
+        floatingBox.style.right= '20%';
         floatingBox.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Color de fondo
-        floatingBox.style.color = '#333'; // Color del texto
+        floatingBox.style.color = '#fff'; // Color del texto
         floatingBox.style.fontFamily = "'Arial', sans-serif"; // Fuente
     } else if (section2Top <= window.innerHeight / 2 && section2Top > -section2.clientHeight / 2) {
-        floatingBox.innerHTML = 'Sección actual: Restaurantes';
+        floatingBox.innerHTML = 'Restaurantes';
         floatingBox.style.top = '50%'; // Cambia la posición más abajo
         floatingBox.style.left = '100px'; // Ajuste de la posición en x
         floatingBox.style.backgroundColor = 'rgba(200, 200, 255, 0.8)'; // Otro color de fondo
         floatingBox.style.color = '#fff'; // Cambia el color del texto a blanco
         floatingBox.style.fontFamily = "'Courier New', monospace"; // Cambia la fuente
     } else if (section3Top <= window.innerHeight / 2 && section3Top > -section3.clientHeight / 2) {
-        floatingBox.innerHTML = 'Sección actual: Tiendas';
+        floatingBox.innerHTML = 'Tiendas';
         floatingBox.style.top = '80%'; // Lo ubicamos en la parte inferior
         floatingBox.style.left = '300px'; // Ajuste de la posición en x
         floatingBox.style.backgroundColor = 'rgba(255, 200, 200, 0.8)'; // Otro color de fondo
